93341cc60dbd1e01678fbe6d449bd79cf7db8be7
vijaysaimutyala <vijsuvig@gmail.com>
2016-01-28 00:11:03 +0530

    This project explains how to auto suggest hash tags based on the multi autocomplete text view

---
+++ app/src/main/java/com/example/vijsu/hashsuggest/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.vijsu.hashsuggest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.Layout;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+    MultiAutoCompleteTextView macv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        macv = (MultiAutoCompleteTextView)findViewById(R.id.multiAutoCompleteTextView);
+        String[] str={"Andoid","Jelly Bean","Froyo",
+                "Ginger Bread","Eclipse Indigo","Eclipse Juno"};
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,str);
+        macv.setThreshold(1);
+        macv.setAdapter(adp);
+        macv.setTokenizer(new MultiAutoCompleteTextView.Tokenizer() {
+            @Override
+            public int findTokenStart(CharSequence text, int cursor) {
+                int i = cursor;
+
+                while (i > 0 && text.charAt(i - 1) != '#') {
+                    i--;
+                }
+
+                //Check if token really started with @, else we don't have a valid token
+                if (i < 1 || text.charAt(i - 1) != '#') {
+                    return cursor;
+                }
+
+                return i;
+            }
+
+            @Override
+            public int findTokenEnd(CharSequence text, int cursor) {
+                int i = cursor;
+                int len = text.length();
+
+                while (i < len) {
+                    if (text.charAt(i) == ' ') {
+                        return i;
+                    } else {
+                        i++;
+                    }
+                }
+
+                return len;
+            }
+
+            @Override
+            public CharSequence terminateToken(CharSequence text) {
+                int i = text.length();
+
+                while (i > 0 && text.charAt(i - 1) == ' ') {
+                    i--;
+                }
+
+                if (i > 0 && text.charAt(i - 1) == ' ') {
+                    return text;
+                } else {
+                    if (text instanceof Spanned) {
+                        SpannableString sp = new SpannableString(text + " ");
+                        TextUtils.copySpansFrom((Spanned) text, 0, text.length(), Object.class, sp, 0);
+                        return sp;
+                    } else {
+                        return text + " ";
+                    }
+                }
+            }
+        });
+        macv.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Layout layout = macv.getLayout();
+                int pos = macv.getSelectionStart();
+                int line = layout.getLineForOffset(pos);
+                int baseline = layout.getLineBaseline(line);
+
+                int bottom = macv.getHeight();
+
+                macv.setDropDownVerticalOffset(baseline - bottom);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
